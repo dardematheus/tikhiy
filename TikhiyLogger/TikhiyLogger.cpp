475782543b9b6df463e417182be81bd182d98b92
dardematheus <matheusdarde9@gmail.com>
2026-05-27 23:40:34 -0300

    Add project files.

---
+++ TikhiyLogger/TikhiyLogger.cpp
@@ -0,0 +1,34 @@
+﻿#include "TikhiyLogger.h"
+
+NTSYSAPI NTSTATUS NtUnmapViewOfSection(IN HANDLE process_handle, IN PVOID base_address);
+
+int 
+main(void)
+{
+	wchar_t executable_path[] = L"C:\\Windows\\System32\\notepad.exe";
+	STARTUPINFOW si;
+	PROCESS_INFORMATION pi;
+	PROCESS_BASIC_INFORMATION pbi_buffer;
+	ZeroMemory(&si, sizeof(si));
+	ZeroMemory(&pi, sizeof(pi));
+	ZeroMemory(&pbi_buffer, sizeof(pbi_buffer));
+	si.cb = sizeof(si);
+
+	if (!CreateProcessW(NULL, executable_path, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+		MessageBoxW(NULL, L"Failed to Create Process", L"ERROR", MB_OK | MB_ICONERROR);
+		return -1;
+	}
+
+	NtQueryInformationProcess(pi.hProcess, ProcessBasicInformation, &pbi_buffer, sizeof(pbi_buffer), NULL);
+	PEB local_peb;
+	SIZE_T bytes_read;
+
+	if (!ReadProcessMemory(pi.hProcess, pbi_buffer.PebBaseAddress, &local_peb, sizeof(local_peb), NULL)) {
+		MessageBoxW(NULL, L"Failed to Read Remote address", L"ERROR", MB_OK | MB_ICONERROR);
+		return -1;
+	}
+
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+	return 0;
+}
---
+++ TikhiyLogger/TikhiyLogger.h
@@ -0,0 +1,9 @@
+﻿// TikhiyLogger.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <winternl.h>
+#include <cstdio>
